849e2dcc221e81c3d7c396f71640fa64403e9b4e
S0IG0 <chibitok.d.s@edu.mirea.ru>
2023-08-12 11:45:14 +0300

    rename upload

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from uplaod.views import FileUploadView
+from upload.views import FileUploadView
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
