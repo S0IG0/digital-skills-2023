eac34625c570218cbb2ac38cb6e1ce986edb9d10
Lefski <93370391+Lefski@users.noreply.github.com>
2023-08-12 11:19:19 +0300

    files upload form added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
+import { useState, ChangeEvent, FormEvent } from 'react'; // Импорт React и необходимых типов
 import danyaLogo from './assets/logo.png';
 import './App.css';
 
 function App() {
-    const handleUpload = () => {
-        // Обработка загрузки данных
+    const [selectedFiles, setSelectedFiles] = useState<File[]>([]); // Указание типа для состояния
+
+    // Обработчик изменения выбранных файлов
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const filesArray = Array.from(event.target.files);
+            setSelectedFiles(filesArray);
+        }
+    };
+
+    // Обработчик отправки формы
+    const handleUpload = (event: FormEvent) => {
+        event.preventDefault();
+        // Обработка загрузки данных, используя selectedFiles
+        console.log(selectedFiles);
+        // Добавьте код для отправки файлов на сервер
+        // POST localhost/upload
+        // файлы с ключом files
     };
 
     return (
         <div className="d-flex flex-column vh-100">
+            {/* Верхняя часть страницы, включая логотип */}
             <nav className="navbar bg-body-tertiary">
                 <div className="container">
                     <a className="navbar-brand">
@@ -15,21 +33,24 @@ function App() {
                     </a>
                 </div>
             </nav>
-            <div className="flex-grow-1 d-flex align-items-center justify-content-center">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h2>Загрузка данных</h2>
-                            <form onSubmit={handleUpload}>
-                                <div className="mb-3">
-                                    <label htmlFor="fileInput" className="form-label">Выберите файл</label>
-                                    <input type="file" className="form-control" id="fileInput" />
-                                </div>
-                                <button type="submit" className="btn btn-primary">Загрузить</button>
-                            </form>
-                        </div>
+            {/* Форма загрузки файлов */}
+            <div className="col-md-6">
+                <h2>Загрузка данных</h2>
+                <form onSubmit={handleUpload}>
+                    <div className="mb-3">
+                        <label htmlFor="fileInput" className="form-label">Выберите файлы</label>
+                        {/* Поле выбора файлов */}
+                        <input
+                            type="file"
+                            className="form-control"
+                            id="fileInput"
+                            multiple
+                            onChange={handleFileChange}
+                        />
                     </div>
-                </div>
+                    {/* Кнопка отправки формы */}
+                    <button type="submit" className="btn btn-primary">Загрузить</button>
+                </form>
             </div>
         </div>
     );
