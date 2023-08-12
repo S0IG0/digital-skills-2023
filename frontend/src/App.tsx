d03530f90f10fedd09bd86666f85b6866dfb315f
Lefski <93370391+Lefski@users.noreply.github.com>
2023-08-12 10:19:23 +0300

    Base UI v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import danyaLogo from './assets/logo.png';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const handleUpload = () => {
+        // Обработка загрузки данных
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="d-flex flex-column vh-100">
+            <nav className="navbar bg-body-tertiary">
+                <div className="container">
+                    <a className="navbar-brand">
+                        <img src={danyaLogo} alt="Bootstrap" width="40" height="40" />
+                    </a>
+                </div>
+            </nav>
+            <div className="flex-grow-1 d-flex align-items-center justify-content-center">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h2>Загрузка данных</h2>
+                            <form onSubmit={handleUpload}>
+                                <div className="mb-3">
+                                    <label htmlFor="fileInput" className="form-label">Выберите файл</label>
+                                    <input type="file" className="form-control" id="fileInput" />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Загрузить</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
